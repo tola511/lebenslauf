81fec0304471d988fd270673d3606506da730213
tola <tola511@icloud.com>
2022-08-07 23:26:17 +0200

    HTML index hinzugefügt.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def index(request):
+    return render(request, 'blog/index.html', {})
